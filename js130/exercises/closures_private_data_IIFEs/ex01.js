c9daeb9963693e04b9114256c645441a53f79f07
Tim <N/A>
2026-03-23 20:07:42 +0000

    submitting js130 closures_private_data_IIFEs ex01

---
+++ js130/exercises/closures_private_data_IIFEs/ex01.js
@@ -0,0 +1,26 @@
+/*
+Function.prototype.bind is a method on all function objects that allows us to hard-bind a function to a particular object. 
+
+The way this works is that you pass a context object to the bind method and 
+it returns a new function that is essentially the same function but hard-bound to the context object supplied.
+
+Create a function myBind, that accepts two arguments: 
+1) The function to bind, 
+2) The context object, and returns a new function that's hard-bound to the passed in context object.
+*/
+
+let interactions = {greeting: 'Hello'};
+function greet() {
+  console.log(this.greeting);
+}
+
+
+function bind(func, contextObj) {
+  return function() {
+    func.call(contextObj);
+  }
+}
+
+let boundGreet = bind(greet, interactions);
+
+boundGreet()
