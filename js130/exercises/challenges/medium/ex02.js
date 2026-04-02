2ee012738e34b0a27a10e126edc2b6b603b02263
Tim <N/A>
2026-04-02 19:43:33 +0100

    moved genName and checkNameUniqueness logic to static

--- js130/exercises/challenges/medium/ex02.js
+++ js130/exercises/challenges/medium/ex02.js
@@ -1,36 +1,43 @@
 class Robot {
-  static names = [];
   static NAME_LETTER_COUNT = 2;
   static NAME_NUM_COUNT = 3;
   static NAME_MAX_NUM = 9;
 
   static ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  
+  static names = [];
+  
+  static checkNameUniqueness(name) {
+    return Robot.names.includes(name);
+  }
+
+  static genName() {
+    let name = '';
+
+    for (let i = 0; i < Robot.NAME_LETTER_COUNT; i += 1) {
+      let randomCharIndex = Math.floor(Math.random() * Robot.ALPHABET.length);
+      name += Robot.ALPHABET[randomCharIndex];
+    }
+
+    for (let i = 0; i < Robot.NAME_NUM_COUNT; i += 1) {
+      name += String(Math.floor(Math.random() * (Robot.NAME_MAX_NUM + 1)));
+    }
+
+    return name;
+  }
 
   constructor() {
     this._name = null;
   }
 
-  genName() {
+  rename() {
     let name;
 
     do {
-      name = '';
-      for (let i = 0; i < Robot.NAME_LETTER_COUNT; i += 1) {
-        let randomCharIndex = Math.floor(Math.random() * (Robot.ALPHABET.length - 1));
-        name += Robot.ALPHABET[randomCharIndex];
-      }
-
-      for (let i = 0; i < Robot.NAME_NUM_COUNT; i += 1) {
-        name += String(Math.floor(Math.random() * (Robot.NAME_MAX_NUM + 1)));
-      }
+      name = Robot.genName();
     }
-    while (Robot.names.includes(name));
+    while(Robot.checkNameUniqueness(name));
 
-    return name;
-  }
-
-  rename() {
-    let name = this.genName();
     this._name = name;
     Robot.names.push(name);
   }
@@ -45,7 +52,11 @@ class Robot {
 
   reset() {
     let nameIndex = Robot.names.indexOf(this._name);
-    Robot.names.splice(nameIndex, 1);
+
+    if (nameIndex !== -1) {
+      Robot.names.splice(nameIndex, 1); // FIX: corrected for case nameIndex === -1
+    }
+    
     this.rename();
   }
 
