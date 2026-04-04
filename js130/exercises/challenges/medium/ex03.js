9d8a32920229104e0f89ecc99461b801ec117be5
Tim <N/A>
2026-04-04 12:57:09 +0100

    submitting js130 exercises challenges medium ex03

---
+++ js130/exercises/challenges/medium/ex03.js
@@ -0,0 +1,61 @@
+class Clock {
+  static HOURS_IN_DAY = 24;
+  static MINS_IN_HOUR = 60;
+  
+  static validHour(hour) {
+    return !Number.isNaN(hour) && hour >= 0 && hour < Clock.HOURS_IN_DAY;
+  }
+
+  static validMin(min) {
+    return !Number.isNaN(min) && min >= 0 && min < Clock.MINS_IN_HOUR;
+  }
+
+  static makeTimeStr(time) {
+    timeStr = String(time);
+    return timeStr.length === 1 ? '0' + time : time;
+  }
+  
+  static at(hour, min = 0) {
+    if (Clock.validHour(hour) && Clock.validMin(min)) {
+      return new Clock(hour, min);
+    } else {
+      throw Error('Invalid time');
+    }
+  }
+
+  static wrapHour(hour) {
+    let days = Math.floor(hour / Clock.HOURS_IN_DAY);
+    return [days, hour - days * Clock.HOURS_IN_DAY];
+  }
+  
+  static wrapMin(min) {
+    let hours = Math.floor(min / Clock.MINS_IN_HOUR);
+    return [hours, min - hours * Clock.MINS_IN_HOUR];
+  }
+
+  constructor(hour, min) {
+     this.hour = hour;
+     this.min = min;
+  }
+
+  add(extraMins) {
+    let [extraHours, wrappedMins] = Clock.wrapMin(this.min + extraMins);
+    let [extraDays, wrappedHours] = Clock.wrapHour(this.hour + extraHours);
+    return new Clock(wrappedHours, wrappedMins);
+  }
+
+  subtract(fewerMins) {
+    let extraMins = Clock.HOURS_IN_DAY * Clock.MINS_IN_HOUR - fewerMins;
+    return this.add(extraMins);
+  }
+
+  toString() {
+    return Clock.makeTimeStr(this.hour) + ':' + Clock.makeTimeStr(this.min);
+  }
+
+  isEqual(otherClock) {
+    return this.hour === otherClock.hour && this.min === otherClock.min;
+  }
+}
+
+module.exports = Clock;
---
+++ js130/exercises/challenges/medium/ex03_pedac.md
@@ -0,0 +1,54 @@
+Create a clock that is independent of date.
+
+You should be able to add minutes to and subtract minutes from the time represented by a given Clock object. Note that you should not mutate Clock objects when adding and subtracting minutes -- create a new Clock object.
+
+Two clock objects that represent the same time should be equal to each other.
+
+You may not use any built-in date or time functionality; just use arithmetic operations.
+
+PEDAC
+
+# PROBLEM
+## Inputs
+- at() method
+  * hours argument
+  * mins argument
+
+- add() method
+  * mins argument
+
+- subtract() method
+
+- toString() method
+
+
+## Behaviors
+- Add mins
+- Subtract mins
+- subtract a few minutes
+- wrap around at midnight
+- wrap around at negative midnight
+- add more than a day
+- subtract more than a day
+- wrap around backwards
+
+## Explicit rules
+- Two clock objects that represent the same time should be equal to each other.
+- You may not use any built-in date or time functionality; just use arithmetic operations.
+
+## Implicit rules
+-
+
+## Questions
+-
+
+# EXAMPLES/TEST CASES
+
+
+# DATA STRUCTURES
+
+
+# ALGORITHM
+
+
+# CODE
